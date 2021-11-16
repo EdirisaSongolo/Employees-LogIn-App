fcd2d94f8d63be9d2358d3034b4a12e80e1cd4fe
Edrissongolo <Edrissongolo@Gmail.com>
2021-11-16 09:40:02 -0500

    final push. demo complete

--- src/main/java/com/loginapp/loginapp/api/HourlyController.java
+++ src/main/java/com/loginapp/loginapp/api/HourlyController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-@RequestMapping("hourlyEmployees")
+@RequestMapping("hourlyemployee")
 @RestController
 public class HourlyController
  {
--- src/main/java/com/loginapp/loginapp/api/SupervisorController.java
+++
@@ -1,9 +0,0 @@
-package com.loginapp.loginapp.api;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class SupervisorController
- {
-
- }
